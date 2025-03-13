31a2f43e7aef6b7fa438d86fea3d0b89875235d0
IKatalevsky <1lyqatalevsk1@gmail.com>
2025-03-13 19:55:22 +0200

    8

---
+++ 13-03-25/task 8-14412.py
@@ -0,0 +1,9 @@
+from itertools import product
+
+cnt = 0
+for i in product('АЛГОРИТМ', repeat=6):
+    i = ''.join(i)
+    if i.count('Л') <= 1 and i[0] != 'Р' and i[-1] in 'АОИ':
+        cnt += 1
+
+print(cnt)

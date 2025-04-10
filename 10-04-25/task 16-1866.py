8bc9ae47718383df6de490ee36bdde773b07be52
IKatalevsky <1lyqatalevsk1@gmail.com>
2025-04-10 19:41:40 +0200

    16, 23

---
+++ 10-04-25/task 16-1866.py
@@ -0,0 +1,13 @@
+from sys import setrecursionlimit
+
+def f(n):
+    if n < 100:
+        return n ** 2
+    if n > 99 and n % 2 == 0:
+        return 0.5 * f(n - 1)
+    if n > 99 and n % 2 == 1:
+        return 2 * f(n - 1)
+
+setrecursionlimit(100000)
+print(1000 * f(16384) / f(7777))
+

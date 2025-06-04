0321fd2d8589fa792fd1882def2fda03eff540c6
Student-10 <1lyqatalevsk1@gmail.com>
2025-06-04 12:58:17 +0300

    shpora

---
+++ shpora/task 6/task 6-17625.py
@@ -0,0 +1,36 @@
+from turtle import *
+
+screensize(10_000, 10_000)
+tracer(0)
+m = 20
+lt(90)
+
+for i in range(10):
+    fd(22 * m)
+    rt(90)
+    fd(16 * m)
+    rt(90)
+up()
+
+fd(1 * m)
+rt(90)
+fd(1 * m)
+lt(90)
+down()
+
+for i in range(10):
+    fd(72 * m)
+    rt(90)
+    fd(79 * m)
+    rt(90)
+up()
+
+for x in range(-30, 30):
+    for y in range(-30, 30):
+        goto(x * m, y * m)
+        dot(8, 'white')
+
+update()
+done()
+
+
---
+++ shpora/task 6/task 6-9829.py
@@ -0,0 +1,30 @@
+from turtle import *
+
+screensize(10_000, 10_000)
+m = 20
+tracer(0)
+lt(90)
+
+rt(90)
+
+for i in range(3):
+    rt(45)
+    fd(10 * m)
+    rt(45)
+
+rt(315)
+fd(10 * m)
+
+for i in range(2):
+    rt(90)
+    fd(10 * m)
+up()
+
+for x in range(-30, 30):
+    for y in range(-30, 30):
+        goto(x * m, y * m)
+        dot(3, 'red')
+
+update()
+done()
+
